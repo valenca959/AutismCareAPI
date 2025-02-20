13113d3ae1a8222184198744b66c4954f0011c76
valenca959 <rodrigovenedino@gmail.com>
2025-02-20 18:03:57 -0300

    att serializers de MedicalHistory e MedicalRecord

--- autismcare/authentication/serializers/parent.py
+++ autismcare/authentication/serializers/parent.py
@@ -6,7 +6,7 @@ class MedicalHistorySerializer(serializers.ModelSerializer):
         model = MedicalHistory
         fields = '__all__'
         extra_kwargs = {
-            'child': {'required': False}  # Remove a obrigatoriedade do campo child
+            'child': {'required': False} 
         }
 
 
@@ -17,17 +17,14 @@ class MedicalRecordSerializer(serializers.ModelSerializer):
         model = MedicalRecord
         fields = '__all__'
         extra_kwargs = {
-            'child': {'required': False}  # Remove a obrigatoriedade do campo child
+            'child': {'required': False} 
         }
 
     def create(self, validated_data):
-        # Remove os dados do histórico médico
         medical_history_data = validated_data.pop('medical_history', [])
         
-        # Cria o MedicalRecord
         medical_record = MedicalRecord.objects.create(**validated_data)
 
-        # Cria os MedicalHistory associados
         for history_data in medical_history_data:
             MedicalHistory.objects.create(child=medical_record.child, **history_data)
 
@@ -41,22 +38,18 @@ class ChildSerializer(serializers.ModelSerializer):
         model = Child
         fields = '__all__'
         extra_kwargs = {
-            'parent': {'required': False}  # Remove a obrigatoriedade do campo parent
+            'parent': {'required': False} 
         }
 
     def create(self, validated_data):
-        # Remove os dados do prontuário médico
         medical_record_data = validated_data.pop('medical_record', None)
         
-        # Cria o Child
         child = Child.objects.create(**validated_data)
 
-        # Cria o MedicalRecord associado, se houver dados
         if medical_record_data:
             medical_history_data = medical_record_data.pop('medical_history', [])
             medical_record = MedicalRecord.objects.create(child=child, **medical_record_data)
 
-            # Cria os MedicalHistory associados
             for history_data in medical_history_data:
                 MedicalHistory.objects.create(child=child, **history_data)
 
@@ -73,66 +66,50 @@ class ParentSerializer(serializers.ModelSerializer):
             'telephone', 'telephone2', 'birthdate', 'cpf', 'spouse', 'children'
         ]
         extra_kwargs = {
-            'password': {'write_only': True}  # A senha não será retornada na resposta
+            'password': {'write_only': True} 
         }
 
     def create(self, validated_data):
-        # Remove os dados dos filhos
         children_data = validated_data.pop('children', [])
         
-        # Cria o Parent
         parent = Parent.objects.create_user(**validated_data)
 
-        # Cria os filhos e seus objetos relacionados
         for child_data in children_data:
-            # Adiciona o parent ao child_data
             child_data['parent'] = parent
             
-            # Remove os dados do prontuário médico
             medical_record_data = child_data.pop('medical_record', None)
             
-            # Cria o Child
             child = Child.objects.create(**child_data)
-
-            # Cria o MedicalRecord associado, se houver dados
             if medical_record_data:
                 medical_history_data = medical_record_data.pop('medical_history', [])
                 medical_record = MedicalRecord.objects.create(child=child, **medical_record_data)
-
-                # Cria os MedicalHistory associados
                 for history_data in medical_history_data:
                     MedicalHistory.objects.create(child=child, **history_data)
 
         return parent
 
     def update(self, instance, validated_data):
-        # Remove os dados dos filhos
         children_data = validated_data.pop('children', [])
         
-        # Atualiza os campos do Parent
         instance = super().update(instance, validated_data)
 
-        # Atualiza ou cria os filhos e seus objetos relacionados
         for child_data in children_data:
             child_id = child_data.get('id', None)
             if child_id:
-                # Atualiza o Child existente
                 child = Child.objects.get(id=child_id, parent=instance)
                 for attr, value in child_data.items():
                     setattr(child, attr, value)
                 child.save()
             else:
-                # Cria um novo Child
                 child_data['parent'] = instance
                 medical_record_data = child_data.pop('medical_record', None)
                 child = Child.objects.create(**child_data)
 
-                # Cria o MedicalRecord associado, se houver dados
                 if medical_record_data:
                     medical_history_data = medical_record_data.pop('medical_history', [])
                     medical_record = MedicalRecord.objects.create(child=child, **medical_record_data)
 
-                    # Cria os MedicalHistory associados
+
                     for history_data in medical_history_data:
                         MedicalHistory.objects.create(child=child, **history_data)
 
