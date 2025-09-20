d0e85d3bfc0ae50d20c86554f8182f4704ff96b8
valenca959 <rodrigovenedino@gmail.com>
2025-09-20 19:39:04 -0300

    Adiciona controle de acesso para visualizações de funcionários, restringindo o acesso a usuários

--- autismcare/authentication/views/employee_view.py
+++ autismcare/authentication/views/employee_view.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 from authentication.models.employee import Employee
 from authentication.serializers.employee_serializer import EmployeeSerializer
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    permission_classes = [IsAdminUser]
